32c635f2864fcbc355aa8ba7acd7cd7c1b2ffe66
Mike Siganos <40475002+MikeSiganos@users.noreply.github.com>
2023-02-28 17:16:35 +0200

    Add lighting manager

---
+++ unipiExUnity/Assets/LightingManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    // Scene References
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    // Variables
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] private bool FollowSystemTime = true;
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+        if (Application.isPlaying)
+        {
+            if (FollowSystemTime)
+            {
+                // Get system time
+                float SysHour = System.DateTime.Now.Hour;
+                float SysMinute = System.DateTime.Now.Minute;
+                float SysMinutePercent = SysMinute / 60; // Get percent 0-59 minute
+                string MinutePercent = SysMinutePercent.ToString("n2"); // Round to 2 decimal points
+                SysMinutePercent = float.Parse(MinutePercent);
+                TimeOfDay = SysHour + SysMinutePercent;
+                TimeOfDay %= 24; // Modulus to ensure always between 0-24
+                UpdateLighting(TimeOfDay / 24f);
+            }
+            else
+            {
+                TimeOfDay += Time.deltaTime;
+                TimeOfDay %= 24; // Modulus to ensure always between 0-24
+                UpdateLighting(TimeOfDay / 24f);
+            }
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        // Set ambient and fog
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+        // If the directional light is set then rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+
+    // Try to find a directional light to use if we haven't set one
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+        // Search for lighting tab sun
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        // Search scene for light that fits criteria (directional)
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
